a4b11e1b00dc29673ea4da4d661de4418a097e66
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2015-08-26 10:19:18 +0000

    adding submenu and new friend controller

--- smartMobi/www/js/app.js
+++ smartMobi/www/js/app.js
@@ -1,15 +1,48 @@
 // it is custom build angular js file
 ess = angular.module("ESS",['ionic'])
 
-ess.controller('ESS-Controller',function(){
-  $scope.submenu = {
-    title : "Ehsan",
-    title : "Mohsin",
-    title : "Malik",
-    title : "Osama",
+.controller('ess-controller', function($scope) {
+  $scope.tasks = [
+    { title: 'Ehsan' },
+    { title: 'Mohsin' },
+    { title: 'Usama' },
+    { title: 'Malik' }
+  ];
+  
+  
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };  
     
-  }
-})
+  
+});
+
+
 
 // Ionic Starter App
 
